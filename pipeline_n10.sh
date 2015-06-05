ae0ab5ff64dfbc751d0edc8f57bb2f6d227083c6
Hao Yu (Cody) <comaniac0422@gmail.com>
2015-06-04 22:10:51 -0700

    Script: golden mode

--- pipeline_n10.sh
+++ pipeline_n10.sh
@@ -6,6 +6,7 @@
 ROOT=/genomics
 
 BWA=$HOME/genomics/bwa-mem-quickassist/bwa-0.7.8/bwa
+BWA_GOLDEN=$HOME/genomics/bwa_golden
 #BWA=$ROOT/bwa-0.7.8/bwa
 #SAMTOOLS=$ROOT/samtools-0.1.19/samtools
 #PICARD=$ROOT/picard-tools-1.79
@@ -18,7 +19,7 @@ DBSNP=$REF/dbsnp-all.vcf
 DBINDEL=$REF/1000G_phase1.indels.b37.vcf
 IDIR=$ROOT/data/HCC1954_small
 INFILE=HCC1954
-ODIR=/tmp/cody_genomics_test
+ODIR=/home/cody
 NTHREAD=40 # n10 has 40 threads
 NCTHREAD=4
 NBATCH_SIZE=10
@@ -75,51 +76,9 @@ WRAPPER() {
 #   samtools faidx $FASTA
 
 # Align sequences with BWA
-#WRAPPER "bwamem" "$BWA mem -t $NTHREAD -Ma -R $HDR $FASTA $IN1 > $SAMFILE"
 rm $SAMFILE
 WRAPPER "bwamem" "$BWA mem -t $NTHREAD -b $NBATCH_SIZE -Ma -R $HDR $FASTA $IN1 > $SAMFILE"
 
-WRAPPER "diff" "diff /tmp/cody_genomics_test/HCC1954_dut.sam /tmp/cody_genomics_test/_${READ_SIZE}HCC1954_dut.sam -q >> flow.log"
-##WRAPPER "bwamem" "$BWA mem -t $NTHREAD -Ma -R $HDR $FASTA $IN1 $IN2 > $SAMFILE"
-##
-### Convert SAM to BAM
-##WRAPPER "samview" "$SAMTOOLS view -@ $NTHREAD -b -t $FAI -S $SAMFILE -o $UNSORTED"
-##
-### TEMP: remove SAM file to free disk space
-##WRAPPER "" "rm $SAMFILE"
-##
-### Sort the BAM
-##WRAPPER "samsort" "$SAMTOOLS sort -@ $NTHREAD -l 5 $UNSORTED $OFILE"
-##
-### Index the sorted BAM
-##WRAPPER "samindex" "$SAMTOOLS index $BAMFILE"
-##
-### Mark duplicates in the BAM
-##WRAPPER "markdup" "java -Xmx8g -jar $PICARD/MarkDuplicates.jar I=$BAMFILE O=$DUPBAM M=$DUPMETRICS TMP_DIR=$TMP VALIDATION_STRINGENCY=SILENT ASSUME_SORTED=true REMOVE_DUPLICATES=true"
+echo Diff: | tee -a flow.log
+diff $SAMFILE ${ODIR}/_${READ_SIZE}HCC1954_dut.sam | grep \">\" | wc -l | tee -a flow.log
 
-### Index the duplicates
-##WRAPPER "dupindex" "$SAMTOOLS index $DUPBAM"
-##
-### Find realignments based on known mutations.
-##WRAPPER "realigner" "java -Xmx8g -jar $GATK -T RealignerTargetCreator -nt $NTHREAD -R $FASTA -o $REALN -known:dbsnp,VCF $DBSNP -known:indels,vcf $DBINDEL -I $DUPBAM"
-##
-### Perform realignments
-##WRAPPER "realigner2" "java -Xmx8g -Djava.io.tmpdir=$TMP -jar $GATK -T IndelRealigner -rf NotPrimaryAlignment -R $FASTA -targetIntervals $REALN -known:indels,vcf $DBINDEL -I $DUPBAM -o $REALNBAM"
-##
-### Index realigned BAM
-##WRAPPER "samindex2" "$SAMTOOLS index $REALNBAM"
-
-# Recalibrate quality scores
-##WRAPPER "recalib" "java -Xmx4g -jar $GATK -T BaseRecalibrator -I $REALNBAM -R $FASTA -nct $NCTHREAD -rf BadCigar -knownSites:mask,vcf $DBSNP -l INFO --default_platform illumina -cov QualityScoreCovariate -cov CycleCovariate -cov ContextCovariate -cov ReadGroupCovariate --disable_indel_quals -o $RECAL"
-##
-### Print all reads with recalibrated scores into final BAM
-##WRAPPER "printreads" "java -Xmx8g -jar $GATK -T PrintReads -R $FASTA -I $REALNBAM -BQSR $RECAL -rf BadCigar -o $FINALBAM"
-##
-### Index final BAM
-##WRAPPER "samindex3" "$SAMTOOLS index $FINALBAM"
-##
-### Compute final stats
-##WRAPPER "flagstat" "$SAMTOOLS flagstat $FINALBAM > $FLAGSTAT"
-##
-### DONE!
-##
