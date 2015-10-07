b2795c93df2fe7c49e6ff624331dacbff467a9c4
peterpengwei <peng.wei.prc@cs.ucla.edu>
2015-10-06 18:57:41 -0700

    Finalize QuickAssist

--- pipeline_ref.sh
+++ pipeline_ref.sh
@@ -2,41 +2,26 @@
 
 # New pipeline as of 2013-10-22.
 
-ROOT=/cdsc_nfs/cdsc0/software/spark
-#BWA=$ROOT/bwa-mem-quickassist/bwa-0.7.8/bwa
-BWA=$ROOT/bwa-0.7.8/bwa
-#SAMTOOLS=$ROOT/samtools-0.1.19/samtools
-#PICARD=$ROOT/picard-tools-1.79
-#GATK=$ROOT/gatk-protected-master/dist/GenomeAnalysisTK.jar
+ROOT=/home/quickassist
+BWA=$ROOT/genomics/bwa-0.7.8/bwa
 
-REF=$ROOT/genomics_data/ReferenceMetadata
+REF=$ROOT/genomics/ReferenceMetadata
 FASTA=$REF/human_g1k_v37.fasta
 FAI=$FASTA.fai
 DBSNP=$REF/dbsnp-all.vcf
 DBINDEL=$REF/1000G_phase1.indels.b37.vcf
-IDIR=$ROOT/genomics_data
+IDIR=$ROOT/genomics/InputFiles
 INFILE=HCC1954
-ODIR=$ROOT/genomics_data
+ODIR=$ROOT/genomics/OutputFiles
 NTHREAD=4
 NCTHREAD=4
-NBATCH_SIZE=10
+NBATCH_SIZE=512
 
 HDR=`printf "'@RG\tID:%s\tLB:%s\tSM:%s'" $INFILE $INFILE $INFILE`
 #IN1=$IDIR/${INFILE}_1_100reads.fq
-IN1=$IDIR/${INFILE}_1_10Mreads.fq
-IN2=$IDIR/${INFILE}_2.fq
+IN1=$IDIR/${INFILE}_1_1Mreads.fq
 OFILE=$ODIR/$INFILE
 SAMFILE=${OFILE}_ref.sam
-UNSORTED=$OFILE.unsorted.bam
-BAMFILE=$OFILE.bam
-DUPBAM=$OFILE.dup.bam
-DUPMETRICS=$OFILE.dup.metrics
-TMP=$ROOT/genomics_data
-REALN=$OFILE.realn.intervals
-REALNBAM=$OFILE.realn.bam
-RECAL=$OFILE.recal.grp
-FINALBAM=$OFILE.final.bam
-FLAGSTAT=$OFILE.flagstat
 
 # Helper function:
 #   $1 : tag
@@ -60,55 +45,8 @@ WRAPPER() {
 	fi
 }
 
-# One-time preparation of reference genome:
-# Create BWA index
-#   $BWA index $FASTA
-# Create FAI index
-#   samtools faidx $FASTA
-
 # Align sequences with BWA
 WRAPPER "bwamem" "$BWA mem -t $NTHREAD -Ma -R $HDR $FASTA $IN1 > $SAMFILE"
-#WRAPPER "bwamem" "$BWA mem -t $NTHREAD -b $NBATCH_SIZE -Ma -R $HDR $FASTA $IN1 > $SAMFILE"
-##WRAPPER "bwamem" "$BWA mem -t $NTHREAD -Ma -R $HDR $FASTA $IN1 $IN2 > $SAMFILE"
-##
-### Convert SAM to BAM
-##WRAPPER "samview" "$SAMTOOLS view -@ $NTHREAD -b -t $FAI -S $SAMFILE -o $UNSORTED"
-##
-### TEMP: remove SAM file to free disk space
-##WRAPPER "" "rm $SAMFILE"
-##
-### Sort the BAM
-##WRAPPER "samsort" "$SAMTOOLS sort -@ $NTHREAD -l 5 $UNSORTED $OFILE"
-##
-### Index the sorted BAM
-##WRAPPER "samindex" "$SAMTOOLS index $BAMFILE"
-##
-### Mark duplicates in the BAM
-##WRAPPER "markdup" "java -Xmx8g -jar $PICARD/MarkDuplicates.jar I=$BAMFILE O=$DUPBAM M=$DUPMETRICS TMP_DIR=$TMP VALIDATION_STRINGENCY=SILENT ASSUME_SORTED=true REMOVE_DUPLICATES=true"
 
-### Index the duplicates
-##WRAPPER "dupindex" "$SAMTOOLS index $DUPBAM"
-##
-### Find realignments based on known mutations.
-##WRAPPER "realigner" "java -Xmx8g -jar $GATK -T RealignerTargetCreator -nt $NTHREAD -R $FASTA -o $REALN -known:dbsnp,VCF $DBSNP -known:indels,vcf $DBINDEL -I $DUPBAM"
-##
-### Perform realignments
-##WRAPPER "realigner2" "java -Xmx8g -Djava.io.tmpdir=$TMP -jar $GATK -T IndelRealigner -rf NotPrimaryAlignment -R $FASTA -targetIntervals $REALN -known:indels,vcf $DBINDEL -I $DUPBAM -o $REALNBAM"
-##
-### Index realigned BAM
-##WRAPPER "samindex2" "$SAMTOOLS index $REALNBAM"
+# DONE!
 
-# Recalibrate quality scores
-##WRAPPER "recalib" "java -Xmx4g -jar $GATK -T BaseRecalibrator -I $REALNBAM -R $FASTA -nct $NCTHREAD -rf BadCigar -knownSites:mask,vcf $DBSNP -l INFO --default_platform illumina -cov QualityScoreCovariate -cov CycleCovariate -cov ContextCovariate -cov ReadGroupCovariate --disable_indel_quals -o $RECAL"
-##
-### Print all reads with recalibrated scores into final BAM
-##WRAPPER "printreads" "java -Xmx8g -jar $GATK -T PrintReads -R $FASTA -I $REALNBAM -BQSR $RECAL -rf BadCigar -o $FINALBAM"
-##
-### Index final BAM
-##WRAPPER "samindex3" "$SAMTOOLS index $FINALBAM"
-##
-### Compute final stats
-##WRAPPER "flagstat" "$SAMTOOLS flagstat $FINALBAM > $FLAGSTAT"
-##
-### DONE!
-##
